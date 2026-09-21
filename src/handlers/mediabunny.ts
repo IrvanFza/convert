69a7c83ca420baaba64ed73b3d96cc4f19ca3235
neko <225498830+neko782@users.noreply.github.com>
2026-09-21 20:50:08 +0200

    lazy load mediabunny encoders

--- src/handlers/mediabunny.ts
+++ src/handlers/mediabunny.ts
@@ -3,9 +3,6 @@ import { Category } from "src/CommonFormats.ts";
 import type { ConvertContext } from "src/ui/ProgressStore.ts";
 import normalizeMimeType from "src/normalizeMimeType.ts";
 import * as mb from "mediabunny";
-import { registerFlacEncoder } from "@mediabunny/flac-encoder";
-import { registerMp3Encoder } from "@mediabunny/mp3-encoder";
-import { registerAacEncoder } from "@mediabunny/aac-encoder";
 
 const FORMATS = new Map<string, [name: string, input: mb.InputFormat, output: mb.OutputFormat]>([
   ["mp4", ["MPEG-4 Part 14", mb.MP4, new mb.Mp4OutputFormat()]],
@@ -22,8 +19,10 @@ const FORMATS = new Map<string, [name: string, input: mb.InputFormat, output: mb
   // ["m3u8", ["HTTP Live Streaming", mb.HLS, new mb.HlsOutputFormat({ segmentFormat: new mb.MpegTsOutputFormat() })]],
 ]);
 
-const anyOrNone = async <T>(items: T[], fn: (item: T) => Promise<boolean>) =>
+const can = async <T>(items: T[], fn: (item: T) => Promise<boolean>) =>
   !items.length || (await Promise.all(items.map((item) => fn(item)))).some(Boolean);
+
+const LAZY_ENCODERS = ["flac", "mp3", "aac"];
 
 class mediabunnyHandler implements FormatHandler {
   public name: string = "mediabunny";
@@ -32,24 +31,17 @@ class mediabunnyHandler implements FormatHandler {
   public offload: boolean = true;
 
   async init() {
-    if (!(await mb.canEncodeAudio("flac"))) {
-      registerFlacEncoder();
-    }
-    if (!(await mb.canEncodeAudio("mp3"))) {
-      registerMp3Encoder();
-    }
-    if (!(await mb.canEncodeAudio("aac"))) {
-      registerAacEncoder();
-    }
-
     for (const [format, [name, input, output]] of FORMATS) {
       const videoCodecs = output.getSupportedVideoCodecs();
       const audioCodecs = output.getSupportedAudioCodecs();
 
-      const canDecodeVideo = await anyOrNone(videoCodecs, mb.canDecodeVideo);
-      const canEncodeVideo = await anyOrNone(videoCodecs, mb.canEncodeVideo);
-      const canDecodeAudio = await anyOrNone(audioCodecs, mb.canDecodeAudio);
-      const canEncodeAudio = await anyOrNone(audioCodecs, mb.canEncodeAudio);
+      const canDecodeVideo = await can(videoCodecs, mb.canDecodeVideo);
+      const canEncodeVideo = await can(videoCodecs, mb.canEncodeVideo);
+      const canDecodeAudio = await can(audioCodecs, mb.canDecodeAudio);
+      const canEncodeAudio = await can(
+        audioCodecs,
+        async (c) => LAZY_ENCODERS.includes(c) || (await mb.canEncodeAudio(c)),
+      );
       let category;
       let from, to;
       const tracks = output.getSupportedTrackCounts();
@@ -90,6 +82,19 @@ class mediabunnyHandler implements FormatHandler {
     if (!mbInputFormat) throw new Error(`could not get format: ${inputFormat.internal}`);
     const mbOutputFormat = FORMATS.get(outputFormat.internal)?.[2];
     if (!mbOutputFormat) throw new Error(`could not get format: ${outputFormat.internal}`);
+    const outAudioCodecs = mbOutputFormat.getSupportedAudioCodecs();
+    if (outAudioCodecs.every((c) => LAZY_ENCODERS.includes(c))) {
+      if (outAudioCodecs.includes("mp3") && !(await mb.canEncodeAudio("mp3"))) {
+        const { registerMp3Encoder } = await import("@mediabunny/mp3-encoder");
+        registerMp3Encoder();
+      } else if (outAudioCodecs.includes("flac") && !(await mb.canEncodeAudio("flac"))) {
+        const { registerFlacEncoder } = await import("@mediabunny/flac-encoder");
+        registerFlacEncoder();
+      } else if (outAudioCodecs.includes("aac") && !(await mb.canEncodeAudio("aac"))) {
+        const { registerAacEncoder } = await import("@mediabunny/aac-encoder");
+        registerAacEncoder();
+      }
+    }
 
     for (const [i, inputFile] of inputFiles.entries()) {
       ctx?.log(`Processing ${inputFile.name}...`);
